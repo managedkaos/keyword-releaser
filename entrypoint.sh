d843a68dbedd9055d10b89af554d1b8b1e3d06d0
Michael <michael@managedkaos.com>
2019-04-06 12:08:32 -0700

    fix output

--- entrypoint.sh
+++ entrypoint.sh
@@ -17,10 +17,12 @@ then
 # a local file with sample data for testing.
 #
 # set EVENT_DATA_PATH = the local file with sample data
+# set LOCAL_TEST = true so a release is not attempted
 #
 elif [ -f ./sample_push_event.json ];
 then
     EVENT_PATH='./sample_push_event.json'
+    LOCAL_TEST=true
 
 #
 # If no file is available for processing, exit with an error
@@ -35,21 +37,37 @@ echo "Processing $EVENT_PATH"
 echo "-----------------------------------------------------------"
 
 # check for the keyword in the event's commit messages
+# use JQ to filter out the messages
+# grep for the keyword in the messages found by JQ
+# `grep -iq` tells grep to ignore case and operate in quiet mode
 if $( jq '.commits[].message, .head_commit.message' < $EVENT_PATH  | grep -iq $* );
 then
     VERSION=$(date +%F.%s)
     DATA=$(printf '{"tag_name":"v%s","target_commitish":"master","name":"v%s","body":"Automated release based on keyword: %s","draft":false,"prerelease":false}' $VERSION $VERSION "$*")
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
 
-    CMD="echo $DATA | http POST $URL"
+    CMD="echo $DATA | http POST $URL | jq ."
 
     echo "Keyword = $*"
+    echo
     echo "Version = $VERSION"
+    echo
     echo "Data    = $DATA"
+    echo
     echo "URL     = $URL"
+    echo
     echo "CMD     = $CMD"
 
-    echo $DATA | http POST $URL | jq .
+    if [ LOCAL_TEST="true" ];
+    then
+        echo
+        echo "##"
+        echo "## [TESTING] No release was created."
+        echo "##"
+        echo
+    else
+        $CMD
+    fi
 else
     echo "Not creating a release because The following keyword was not present: $*"
 fi
