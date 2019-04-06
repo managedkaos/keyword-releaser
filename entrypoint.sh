b525882edd666288895eb851ab622728b99186e9
Michael <michael@managedkaos.com>
2019-04-06 15:14:00 -0700

    Use multiple line for DATA to make is easier to read

--- entrypoint.sh
+++ entrypoint.sh
@@ -50,7 +50,14 @@ jq . < $EVENT_PATH
 if jq '.commits[].message, .head_commit.message' < $EVENT_PATH | grep -i -q "$*";
 then
     VERSION=$(date +%F.%s)
-    DATA=$(printf '{"tag_name":"v%s","target_commitish":"master","name":"v%s","body":"Automated release based on keyword: %s","draft":false,"prerelease":false}' "$VERSION" "$VERSION" "$*")
+
+    DATA="$(printf '{"tag_name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"target_commitish":"master",')"
+    DATA="${DATA} $(printf '"name":"v%s",' $VERSION)"
+    DATA="${DATA} $(printf '"body":"Automated release based on keyword: %s",' "$*")"
+    DATA="${DATA} $(printf '"draft":false, "prerelease":false}')"
+
+    #DATA=$(printf '{"tag_name":"v%s","target_commitish":"master","name":"v%s","body":"Automated release based on keyword: %s","draft":false,"prerelease":false}' "$VERSION" "$VERSION" "$*")
     URL="https://api.github.com/repos/${GITHUB_REPOSITORY}/releases?access_token=${GITHUB_TOKEN}"
 
     CMD="echo $DATA | http POST $URL | jq ."
